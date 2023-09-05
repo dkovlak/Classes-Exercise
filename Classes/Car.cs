9cf4ec4c556168b9a397a3f5bad035304988b429
David Kovlak <kovlakdavid4@gmail.com>
2023-09-04 18:47:34 -0700

    Car

---
+++ Classes/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Classes
+{
+	public class Car
+	{
+        public Car()
+        {
+
+        }
+
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int Year { get; set; }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+    }
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car1 = new Car();
+
+            car1.Make = "BMW";
+            car1.Model = "M3";
+            car1.Year = 2023;
+
+            Car car2 = new Car("Nissan", "GTR", 2017);
+
+            Car car3 = new Car
+            {
+                Make = "Mercedes",
+                Model = "220",
+                Year = 2022
+            };
+
+            var carList = new List<Car>() { car1, car2, car3 };
+
+            foreach (var each in carList)
+            {
+                if (each == car1) { Console.WriteLine($"First car: {each.Make} {each.Model} {each.Year}"); }
+                if (each == car2) { Console.WriteLine($"Second car: {each.Make} {each.Model} {each.Year}"); }
+                if (each == car3) { Console.WriteLine($"Third car: {each.Make} {each.Model} {each.Year}"); }
+            }
+
         }
     }
 }
